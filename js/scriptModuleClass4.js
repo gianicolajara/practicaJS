020b59def4208bd435a1fd1953856e58e6d700f9
gianicolajara <66574963+gianicolajara@users.noreply.github.com>
2020-12-08 15:04:52 -0400

    Update scriptModuleClass4.js

--- js/scriptModuleClass4.js
+++ js/scriptModuleClass4.js
@@ -1,11 +1,13 @@
 export const primo = n => {
     if(n && typeof n === 'number' && n > 1){
         let count = 0;
-
-        for(let i = 1; i <= n ; i++)
-            if(n%i === 0) count += 1
-
-        if(count > 2)return 'No es primo'
+        for(let i = 2; i <= n ; i++){
+            if(n%i === 0){
+                count += 1
+                if(count > 1) break;
+            }
+        }
+        if(count > 1)return 'No es primo'
         else return 'Es primo'
 
     }else return 'invalido'
