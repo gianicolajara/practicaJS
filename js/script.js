6f9f5cd66055847b93183844b72ed80f3218725c
gianicola <gianicolajara@gmail.com>
2020-12-08 10:04:43 -0400

    practica

---
+++ js/script.js
@@ -0,0 +1,3 @@
+import { revertirPalabra, contarPalabras, palindromo, eliminarPalabra } from './scriptModule.js'
+
+console.log(eliminarPalabra('xyz1, xyz2, xyz3, xyz4 y xyz5', 'xyz'));
---
+++ js/scriptModule.js
@@ -0,0 +1,33 @@
+export const revertirPalabra = p => (p && typeof p === 'string' && p.length > 0)
+                                        ? p.split('').reverse().join('')
+                                        : 'Invalido'
+
+export const contarPalabras = (p, b) => {
+    let reg = null;
+    return ((p && typeof p === 'string' && p.length > 0) && (b && typeof b === 'string' && b.length > 0))
+        ? (
+            reg = RegExp(`${b}{0,}`, 'gi'),
+            p.match(reg).length
+        ) :(
+            'invalido'
+        )
+}
+
+export const palindromo = p => {
+    return (p && p === 'string' && p.length > 0) 
+        ?
+            (
+            newP = p.normalize('NFD').replace(/[\u0300-\u036f,. ]/g, "").toLowerCase(),
+            newP === newP.split('').reverse().join('')
+            )
+        : 'invalido'
+}
+
+export const eliminarPalabra = (p, l) => {
+    let reg = null;
+    return ((p && typeof p === 'string' && p.length > 0) && (l && p.length > 0))
+        ? (
+            reg = RegExp(`${l}`, 'g'),
+            p.replace(reg, "")
+        ): 'invalido'
+}
