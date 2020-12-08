d9b6e96fb72c2b161d627480f5542b2e89ebbd46
gianicola <gianicolajara@gmail.com>
2020-12-08 14:41:05 -0400

    ejercicios clase 4

---
+++ js/scriptModuleClass4.js
@@ -0,0 +1,23 @@
+export const primo = n => {
+    if(n && typeof n === 'number' && n > 1){
+        let count = 0;
+
+        for(let i = 1; i <= n ; i++)
+            if(n%i === 0) count += 1
+
+        if(count > 2)return 'No es primo'
+        else return 'Es primo'
+
+    }else return 'invalido'
+}
+
+export const imparPar = n => (n && typeof n === 'number' && n > 0)
+                                ? (n%2===0) ? 'Par' : 'Impar'
+                                : 'Invalido'
+
+export const temp = (n, m) => ((n && typeof n === 'number') 
+                                && (m && typeof m === 'string' 
+                                && (m === 'C' || m === 'F')))
+                                ? (m === 'C') ? (n * (9/5)) + 32 : (n - 32) * 5/9
+                                : 'Invalido'
+                            
