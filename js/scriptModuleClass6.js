7dd44aef84f0f0c3719a67704ec274f0e6e0005d
gianicola <gianicolajara@gmail.com>
2020-12-09 18:47:49 -0400

    modificaciones clase 6

--- js/scriptModuleClass6.js
+++ js/scriptModuleClass6.js
@@ -1,11 +1,11 @@
 export const letras = p => (p && typeof p === 'string') 
-                               ? `Vocales ${p.match(/[aeiou]/gi).length} consonantes ${p.match(/[b-df-hj-np-tv-z]/gi).length}`
+                               ? `Vocales ${p.match(/[aeiou]/gi).length} consonantes ${p.match(/[b-df-hj-np-tv-zñ]/gi).length}`
                                : 'invalido'
                     
-export const nombreValido = n => (n && typeof n === 'string')
+export const nombreValido = n => (n && typeof n === 'string' && n.length > 0)
                                     ? (RegExp(/^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[\s]*)+$/, 'g').test(n))
                                     : 'Invalido'
 
-export const emailValido = e => (e && typeof e === 'string')
+export const emailValido = e => (e && typeof e === 'string' && n.length > 0)
                                     ?(RegExp(/^[\w.]+\@+[a-z0-9]+\.[a-z]{2,3}$/, 'g').test(e))
                                     : 'Invalido'
