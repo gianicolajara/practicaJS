096aea15b01979579abd62ee65b4649bfd31a6f2
gianicola <gianicolajara@gmail.com>
2020-12-11 11:01:51 -0400

    cambios clase 7

--- js/scriptModuleClass7.js
+++ js/scriptModuleClass7.js
@@ -1,15 +1,30 @@
-export const elevar = n => (n && n instanceof Array && n.length > 0)
-                                ? n.map((v) => Math.pow(v,2))
-                                : 'invalido'
+export const elevar = n => {
+    if(n && n instanceof Array && n.length > 0){
+       for(let number of n){
+           if(typeof number !== 'number') return `${number} no es un numero`
+       }
+       return n.map((v) => Math.pow(v,2))
+    }else return 'invalido'  
+}
+                                
 
-export const maxMin = n => (n && n instanceof Array && n.length > 0)
-                                ? [Math.max(...n), Math.min(...n)]
-                                : 'invalido'
+export const maxMin = n => {
+    if(n && n instanceof Array && n.length > 0){
+        for(let number of n)
+            if(typeof number !== 'number') return `${number} no es un numero`
+        
+        return [Math.max(...n), Math.min(...n)]
+    }else return 'invalido'      
+}
 
 export const parImpar = n => {
     if(n && n instanceof Array && n.length > 0){
-        const pares = [], impares = [];
-        n.forEach((v) => (v%2===0) ? pares.push(v) : impares.push(v))
-        return { pares, impares }
+        for(let number of n)
+            if(typeof number !== 'number') return `${number} no es un numero`
+
+        return {
+            pares: n.filter((v) => v%2==0),
+            impares: n.filter((v) => v%2!==0)
+        }        
     }
-}
+}   
